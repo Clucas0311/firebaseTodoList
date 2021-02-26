7f6cb350c236f2cc7b93aa7505e449b185ebea3c
Clucas0311 <clucas0311@gmail.com>
2021-02-25 20:25:04 -0500

    Feat: Added TodoList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+	const [todoList, setTodoList] = useState(['Take dogs for a Walk', 'Go To The Gym']);
 	return (
 		<div className="App">
 			<h1>Hello World</h1>
 			<input />
 			<button>Add Todo</button>
 			<ul>
-				<li>Walk The dog</li>
-				<li>Throw away the trash</li>
+				{todoList.map((todoItem) => (
+					<li>{todoItem}</li>
+				))}
 			</ul>
 		</div>
 	);
