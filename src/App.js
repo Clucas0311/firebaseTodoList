50807b17b60aa093aa6f43f4bf5dc4769c84b00e
Clucas0311 <clucas0311@gmail.com>
2021-02-25 20:07:58 -0500

    Firebase ToDo List

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+			<input />
+			<button>Add Todo</button>
+			<ul>
+				<li>Walk The dog</li>
+				<li>Throw away the trash</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
