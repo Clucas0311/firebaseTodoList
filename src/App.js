ede5430ebef4bf3a2fd98f0ca8651bedf38a8484
Clucas0311 <clucas0311@gmail.com>
2021-02-25 20:46:03 -0500

    CFeat clear input box

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import './App.css';
 
 function App() {
 	const [todoList, setTodoList] = useState(['Take dogs for a Walk', 'Go To The Gym']);
+	const [input, setInput] = useState('');
+
+	const addTodo = (event) => {
+		event.preventDefault();
+		console.log('Working');
+		setTodoList([...todoList, input]);
+		// to clear the input box
+		setInput('');
+	};
 	return (
 		<div className="App">
 			<h1>Hello World</h1>
-			<input />
-			<button>Add Todo</button>
-			<ul>
-				{todoList.map((todoItem) => (
-					<li>{todoItem}</li>
-				))}
-			</ul>
+			<form action="">
+				<input value={input} onChange={(event) => setInput(event.target.value)} />
+				<button onClick={addTodo}>Add Todo</button>
+				<ul>
+					{todoList.map((todoItem) => (
+						<li>{todoItem}</li>
+					))}
+				</ul>
+			</form>
 		</div>
 	);
 }
